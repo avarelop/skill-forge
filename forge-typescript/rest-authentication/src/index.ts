e405cca41160de0deaa1bd77b10321f999eceea4
Alvaro Valera <137428849+avarelop@users.noreply.github.com>
2025-03-04 00:09:19 +0100

    feat: create fist controller for auth endpoints

--- forge-typescript/rest-authentication/src/index.ts
+++ forge-typescript/rest-authentication/src/index.ts
@@ -1,12 +1,14 @@
 import { env } from "./config";
+import { AppRoutes } from "./presentation/router";
 import { Server } from "./presentation/server";
 
 async function main() {
   const options = {
     port: env.PORT,
+    router: AppRoutes.routes,
   };
-  
   const server = new Server(options);
   await server.start();
 }
+
 main();
