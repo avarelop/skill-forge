d820092c93e36772893bfd40333a01f425a62d39
Alvaro Valera <137428849+avarelop@users.noreply.github.com>
2025-03-03 21:55:21 +0100

    feat: add initial server configuration and health check

--- forge-typescript/rest-authentication/src/index.ts
+++ forge-typescript/rest-authentication/src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello World");
+import { env } from "./config";
+import { Server } from "./presentation/server";
+
+async function main() {
+  const options = {
+    port: env.PORT,
+  };
+  
+  const server = new Server(options);
+  await server.start();
+}
+main();
