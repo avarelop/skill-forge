cef84d1bb8496e877bda3f5d93e5b9ae6831aad5
Alvaro Valera <137428849+avarelop@users.noreply.github.com>
2025-03-21 13:43:20 +0100

    feat(starter-project): update books interactions

--- forge-htmx/starter-project/app.js
+++ forge-htmx/starter-project/app.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import BOOKS_DATA from './data/data.js';
+import createBookTemplate from './views/book.js';
 import createHomePageTemplate from './views/index.js';
 import listBooksTemplate from './views/listBooks.js';
 
@@ -16,6 +18,19 @@ app.get('/', (req, res) => {
 
 app.get('/books', (req, res) => {
   res.send(listBooksTemplate());
+});
+
+app.post('/books', (req, res) => {
+  const { title, author } = req.body;
+  const newBook = { id: Math.random().toString(), title, author };
+  BOOKS_DATA.push(newBook);
+  res.redirect(`/books/${newBook.id}`);
+});
+
+app.get('/books/:id', (req, res) => {
+  const { id } = req.params;
+  const book = BOOKS_DATA.find(book => book.id === id);
+  res.send(createBookTemplate(book));
 });
 
 // listen to port
