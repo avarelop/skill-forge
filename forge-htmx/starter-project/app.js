5af17b1c3923df15682b53047da82735f080051f
Alvaro Valera <137428849+avarelop@users.noreply.github.com>
2025-03-22 08:31:50 +0100

    feat(starter-project): add update book functionallity

--- forge-htmx/starter-project/app.js
+++ forge-htmx/starter-project/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import BOOKS_DATA from './data/data.js';
 import createBookTemplate from './views/book.js';
+import createEditBookTemplate from './views/editBook.js';
 import listBooksTemplate from './views/listBooks.js';
 
 // create app
@@ -32,11 +33,33 @@ app.get('/books/:id', (req, res) => {
   res.send(createBookTemplate(book));
 });
 
+app.put('/books/:id', (req, res) => {
+  const { id } = req.params;
+  const { title, author } = req.body;
+
+  const idx = BOOKS_DATA.findIndex(book => book.id === id);
+  
+  if (idx === -1) {
+    return res.status(404).send('Book not found');
+  }
+
+  BOOKS_DATA[idx].title = title;
+  BOOKS_DATA[idx].author = author;
+
+  res.send(createBookTemplate(BOOKS_DATA[idx]));
+});
+
 app.delete('/books/:id', (req, res) => {
   const { id } = req.params;
   const idx = BOOKS_DATA.findIndex(book => book.id === id);
   BOOKS_DATA.splice(idx, 1);
   res.send(listBooksTemplate());
+});
+
+app.get('/books/:id/edit', (req, res) => {
+  const { id } = req.params;
+  const book = BOOKS_DATA.find(book => book.id === id);
+  res.send(createEditBookTemplate(book));
 });
 
 // listen to port
