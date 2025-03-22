82de3e05b7d083b0dcb519225c67d303074f9fca
Alvaro Valera <137428849+avarelop@users.noreply.github.com>
2025-03-22 23:41:44 +0100

    feat(starter-project-v2): add mustache configuration and templates

--- forge-htmx/net-ninja/starter-project-v2/src/app.js
+++ forge-htmx/net-ninja/starter-project-v2/src/app.js
@@ -1,11 +1,37 @@
 import express from 'express';
+import mustache from 'mustache-express';
+import BOOKS_DATA from '../data/data.js';
+
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
 
+// Set up mustache as the template engine
+app.engine('html', mustache());
+app.set('view engine', 'html');
+app.set('views', './public');
+
 app.get('/', (req, res) => {
   res.send('index.html');
+});
+
+app.get('/books', (_, res) => {
+  console.log('This is a test inside the books route');
+  res.render('templates/bookList.template.html', { books: BOOKS_DATA });
+});
+
+app.get('/books/:id', (req, res) => {
+  const { id } = req.params;
+  const idx = BOOKS_DATA.findIndex(book => book.id === id);
+
+  if (idx === -1) {
+    res.status(404).send('Book not found');
+    return;
+  }
+
+  const book = BOOKS_DATA[idx];
+  res.render('templates/book.template.html', { book });
 });
 
 app.listen(3000, () => {
