bfdabea64e3486ae326b62bc8720d028b8fc93ff
Alvaro Valera <137428849+avarelop@users.noreply.github.com>
2025-03-23 03:46:10 +0100

    feat(starter-project-v2): add edit book functionality

--- forge-htmx/net-ninja/starter-project-v2/src/app.js
+++ forge-htmx/net-ninja/starter-project-v2/src/app.js
@@ -17,7 +17,15 @@ app.get('/', (req, res) => {
 });
 
 app.get('/books', (_, res) => {
+  console.log('books');
   res.render('templates/bookList.template.html', { books: BOOKS_DATA });
+});
+
+app.get('/books/new', (_, res) => {
+  res.render('templates/bookForm.template.html', {
+    isEdit: false,
+    book: { title: '', author: '' }
+  });
 });
 
 app.get('/books/search', (req, res) => {
@@ -36,7 +44,7 @@ app.get('/books/search', (req, res) => {
   res.render('templates/bookList.template.html', { books: filteredBooks });
 });
 
-app.get('/books/:id/edit', (req, res) => {
+app.get('/books/edit/:id', (req, res) => {
   const book = BOOKS_DATA.find(book => book.id === req.params.id);
   res.render('templates/bookForm.template.html', {
     isEdit: true,
@@ -44,11 +52,16 @@ app.get('/books/:id/edit', (req, res) => {
   });
 });
 
-app.get('/books/new', (_, res) => {
-  res.render('templates/bookForm.template.html', {
-    isEdit: false,
-    book: { title: '', author: '' }
-  });
+app.delete('/books/:id', (req, res) => {
+  const { id } = req.params;
+  const idx = BOOKS_DATA.findIndex(book => book.id === id);
+
+  if (idx === -1) {
+    res.status(404).send('Book not found');
+    return;
+  }
+
+  res.render('templates/bookList.template.html', { books: BOOKS_DATA.splice(idx, 1) });
 });
 
 app.listen(3000, () => {
